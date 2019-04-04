95d0f70e173d933f63638ea88c448d8ebcb8cd14
Damian Ruiz <ruiz.damian17@gmail.com>
2019-04-04 16:54:07 -0700

    Added print statements for the two message variables

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,3 +4,6 @@ let cost: Double = 5.50
 
 let message: String = "The total price is $" + String(cost)
 let otherMessage: String = "The other total price is $\(cost)"
+
+print(message)
+print(otherMessage)
