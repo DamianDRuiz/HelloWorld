5af998a910a2d699e58e89be95ea6628df7c638c
Damian Ruiz <ruiz.damian17@gmail.com>
2019-04-04 17:44:47 -0700

    Added a comment after new breakfast options

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -13,8 +13,9 @@ let wakeUpTime: Int = 8
 if wakeUpTime == 6 {
     print("Cool good job lol")
 } else if wakeUpTime == 7 {
-    print("Welp make some toast or whateve??") 
+    print("Welp make some toast or whateve??")
 } else {
     print("Wow lmao enjoy eating beef jerky for breakfast idiot lol")
 }
 
+//Commenting something!
