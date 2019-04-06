f5e59b78ef0704f50bdac8509ba8c696b32ff1e1
Damian Ruiz <ruiz.damian17@gmail.com>
2019-04-06 16:02:31 -0700

    HelloEmoji Project Started

---
+++ HelloEmojis/HelloEmojis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloEmojis
+//
+//  Created by Damian Ruiz on 4/6/19.
+//  Copyright © 2019 Big D Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
